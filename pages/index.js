c270c04106885b12cb6db9a3d8145350beef4a12
Eren Dalçık <dalcikferhateren@gmail.com>
2023-12-28 17:37:33 +0300

    add links and new sections

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,12 @@ import {
   Heading,
   Image,
   useColorModeValue,
-  Button
+  Button,
+  SimpleGrid,
+  List,
+  ListItem,
+  Link,
+  Icon
 } from '@chakra-ui/react'
 
 import NextLink from 'next/link'
@@ -13,14 +18,25 @@ import Layout from '../components/layouts/article'
 import Paragraph from '../components/paragraph'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
+import { GridItem } from '../components/grid-item'
+import { IoLogoTwitter, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
 
 const Page = () => {
   return (
     <Layout>
       <Container>
-        <Box borderRadius="lg" bg="#ffffff40" p={4} mb={9} align="center">
+        <Box
+          borderRadius="lg"
+          bg="#ffffff40"
+          p={4}
+          mb={9}
+          my={15}
+          align="center"
+        >
           Welcome to my website
         </Box>
+
+        
         <Box display={{ md: 'flex' }}>
           <Box flexGrow={1}>
             <Heading as="h2" variant="page-title">
@@ -32,7 +48,7 @@ const Page = () => {
           </Box>
           <Box
             flexShrink={0}
-            mt={{ base: 4, md: 0 }}
+            mt={{ base: 8, md: 0 }}
             ml={{ md: 6 }}
             align="center"
           >
@@ -48,6 +64,50 @@ const Page = () => {
             ></Image>
           </Box>
         </Box>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Links
+          </Heading>
+
+          <List>
+            <ListItem>
+              <Link
+                href="https://www.linkedin.com/in/erendalcik/"
+                target="_blank"
+              >
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoLinkedin} />}
+                >
+                  erendalcik
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://github.com/ferend" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoGithub} />}
+                >
+                  ferend
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://twitter.com/erendalcik" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoTwitter} />}
+                >
+                  erendalcik
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+        </Section>
         <Section delay={0.1}>
           <Heading as="h3" variant="section-title">
             Work
@@ -74,6 +134,7 @@ const Page = () => {
             </Button>
           </Box>
         </Section>
+        
         <Section delay={0.2}>
           <Heading as="h3" variant="section-title">
             Bio
@@ -94,6 +155,55 @@ const Page = () => {
               <a style={{ color: 'teal', textDecoration: 'underline' }}>
                 Azerion
               </a>
+            </NextLink>
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Certifications
+          </Heading>
+          <Paragraph>
+            <Image
+              my={4}
+              display="inline-block"
+              borderRadius="full"
+              src="/images/contents/badge.svg"
+              alt="Profile Img"
+            ></Image>
+          </Paragraph>
+
+          <BioSection>
+            <BioYear>•</BioYear>
+            Business Metrics for Data-Driven Companies{' '}
+            <NextLink
+              legacyBehavior
+              href="https://www.coursera.org/account/accomplishments/verify/FS8ELVXGH8KB?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course"
+              passHref
+            >
+              <a style={{ color: 'teal' }}>(Credential)</a>
+            </NextLink>
+          </BioSection>
+
+          <BioSection>
+            <BioYear>•</BioYear>
+            Technical Support Training by Google{' '}
+            <NextLink
+              legacyBehavior
+              href="https://www.coursera.org/account/accomplishments/verify/HQTUVRZVAFAG?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=pdf_header_button&utm_product=course"
+              passHref
+            >
+              <a style={{ color: 'teal' }}>(Credential)</a>
+            </NextLink>
+          </BioSection>
+          <BioSection>
+            <BioYear>•</BioYear>
+            IBM Data Science Orientation{' '}
+            <NextLink
+              legacyBehavior
+              href="https://www.credly.com/badges/a66754f0-64a5-4220-8d2d-d7e153640737/linked_in"
+              passHref
+            >
+              <a style={{ color: 'teal' }}>(Credential)</a>
             </NextLink>
           </BioSection>
         </Section>
